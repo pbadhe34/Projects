e9c4e557d4c9fbf97662b4b3e956e619d702d707
Prakash Badhe <prakash.badhe@agilesoft.in>
2017-09-19 11:21:43 +0530

    First commit

---
+++ Java-App/src/com/data/User.java
@@ -0,0 +1,10 @@
+package com.data;
+
+public class User {
+	
+	public void run()
+	{
+		System.out.println("User.run()");
+	}
+
+}
---
+++ Java-App/src/com/org/Test.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import com.data.User;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 User obj = new User();
+		 obj.run();
+		 System.out.println("The main in Test");
+	}
+
+}
